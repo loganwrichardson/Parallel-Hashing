6ec54eeb7c53a69f7f4658e6756daeae0ed167fb
loganwrichardson <richardsonlw@appstate.edu>
2023-07-21 11:51:25 -0400

    structs, init, hash_func, insert, and find

--- hashing.c
+++ hashing.c
@@ -1,6 +1,4 @@
-//
 // Created by Logan Richardson and Vidhi Patel on 7/20/23.
-//
 
 #include "helpers.h"
 #include "macros.h"
@@ -8,88 +6,138 @@
 #include "producerConsumer1.h"
 #include "produceConsumer2.h"
 #include "BSTversions.h"
-#include "hasing.h"
+#include "hashing.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define SIZE_DEFAULT 1000;
-int hash_table_size = SIZE_DEFAULT * SIZE_DEFAULT
+// Number of values to insert
+#define NUM_NODES 1000
 
-struct Data {
-    int val;
-    int key;
-};
-
-typedef struct {
-    HashEntry entries[HASH_TABLE_SIZE]
-} HashTable;
-
-static void insert(int key);
-static void remove(int key);
-static void find(int key, int * HashTable);
+// Size of the hash table: NUM_NODES squared
+int hash_table_size = NUM_NODES * NUM_NODES;
 
 /**
- * h(k) = ((ak + b) mod p) mod m (p is prime or relatively prime with m; Universal
- * Hash Function.
+ * Node for storing each data point
+ * Uses linked list chaining to resolve collisions
+ * key: the hash key for the data point
+ * value: the data to be stored
+ * node * next: next node for the linked list, null if not needed
+ */
+typedef struct node_t {
+    int key;
+    int value;
+    struct node * next;
+} node;
+
+/**
+ * Structure for creating a linked list
+ * Uses: hash table and chaining (collision resolution)
+ */
+typedef struct linked_list_t {
+    // The head of the linked list
+    struct node * head;
+    // The tail of the linked list
+    struct node * tail;
+} linked_list;
+
+// Hash Table
+linked_list * hash_table[hash_table_size];
+// Number of nodes in the hash table
+int size = 0;
+// Maximum size of the chaining (collision resolution) array
+int max = 10;
+
+// Function Declarations
+void insert(int key, int data);
+void remove(int key);
+int find(int key, linked_list hash_table);
+void init_hash_table();
+
+/**
+ * Universal Hash Function
+ * h(k) = ((ak + b) mod p) mod m (p is prime or relatively prime with m;
  * @param key: int
  * @return int
  */
-int hashFunc(int key) {
+int hash_func(int key) {
     return (int)((2971 * key + 101923) % 128189) % hash_table_size
 }
 
-struct Data * find(int key) {
-    int hashIndex = hashFunct(key);
-
-    while(HashTable->entries[hashIndex] != NULL) {
-        if(hashArray[hashIndex]->key == key)
-            return hashAr
-    }
-}
-
 /**
- * Insert.
+ * Insert
  * Inserts an entry into the hash table.
  * @param key: int
  * @param data: struct
  */
 void insert(int key, int data) {
 
-    struct Data *item
-}
+    // Keeps track of if you need to rehash
+    float n = 0.0;
 
-// Insert an element into the hash table using linear probing for collisions.
-void insertHashTable(HashTable* table, int key, int val) {
-    int index = hashFunc(key);
+    // Find the hash index.
+    int hash_index = hash_func(key);
 
-    while (table->entries[index].key != -1) {
-        index = (index + 1) % HASH_TABLE_SIZE; // Linear probing
+    // Store the linked list from the hash table index
+    linked_list * list = (struct node *) array[index].head;
+
+    node * temp = (struct node *) malloc(sizeOf(struct node));
+    temp->key = key;
+    temp->value = data;
+    temp->next = NULL;
+
+    if (list == NULL) {
+        // No linked list is present at the node.
+        hash_array[hash_index].head = temp;
+        hash_array[hash_index].tail = temp;
+        size++;
     }
-
-    table->entries[index].key = key;
-    table->entries[index].value = value;
+    else {
+        // There is a linked list present at the hash table node
+        int find_index = find(key, *list);
+        if (find_index == -1) {
+            // The key is not already present in the chaining linked list
+            hash_array[index].tail->next = temp;
+            hash_array[index].tail = temp;
+            size + ;
+        } else {
+            // The key is present in the chaining linked list
+            // Update the value of the existing key
+            //TODO: consider ... is this the best thing to do here?
+            //TODO: will this just overwrite the existing node?
+            node * pre_existing_node = find(find_index, list);
+            pre_existing_node->value = data;
+        }
+    }
+    //TODO: refactor this
+    //TODO: consider -- maybe we don't need this functionality
+    n = (1.0 * size) / max;
+    if (n >= 0.75) {
+        // rehashing
+        rehash();
+    }
 }
 
 /**
- * To initiaze the hash table
+ * Initialize the hash table
  */
-void initHashTable(HashTable* table) {
-    for (int i = 0; i < HASH_TABLE_SIZE; i++) {
-        table->entries[i].key = -1;
+void init_hash_table() {
+    int k = 0;
+    for (k = 0; k < hash_table_size; k++) {
+        hash_table[k].head = NULL;
+        hash_table[k].tail = NULL;
     }
 }
 
 /**
- * Remove.
+ * Remove
  * Removes an entry from the hash table.
  * @param value: struct Data
  */
-void remove(struct Data * value) {
+void remove(struct node * value) {
     int key = value->key;
-    int hashIndex = hashFunct(key);
-
+    int hashIndex = hash_func(key);
 }
 
 //struct node * find(int key) {
