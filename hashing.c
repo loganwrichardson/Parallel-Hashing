0531442033d679abe2ae19e3d2223c6c140cd55b
loganwrichardson <richardsonlw@appstate.edu>
2023-07-23 12:29:08 -0400

    Fixed many compile issues.

--- hashing.c
+++ hashing.c
@@ -11,7 +11,7 @@
 #include "macros.h"
 #include "wrappers.h"
 #include "producerConsumer1.h"
-#include "produceConsumer2.h"
+#include "producerConsumer2.h"
 #include "BSTversions.h"
 #include "hashing.h"
 #include <stdio.h>
@@ -22,7 +22,7 @@
 // Number of values to insert
 #define NUM_NODES 1000
 // Size of the hash table: NUM_NODES squared
-int hash_table_size = NUM_NODES * NUM_NODES;
+#define HASH_TABLE_SIZE (NUM_NODES * NUM_NODES)
 
 /**
  * Node structure for storing each data point
@@ -30,37 +30,36 @@ int hash_table_size = NUM_NODES * NUM_NODES;
  * value: the data to be stored
  * node * next: next node for the linked list, null if not needed
  */
-typedef struct node_t {
+struct node {
     int key;
     int value;
-    struct node * next;
-} node;
+    struct node *next;
+};
 
 /**
  * Structure for creating a linked list
  * Used for the outer level hash table as well as node chaining
  */
-typedef struct linked_list_t {
+struct hash_table_item {
     // The head of the linked list
-    struct node * head;
+    struct node *head;
     // The tail of the linked list
-    struct node * tail;
-} linked_list;
+    struct node *tail;
+};
 
 // Hash Table
-linked_list * hash_table[hash_table_size];
+struct hash_table_item *hash_table;
 // Number of nodes in the hash table
 int size = 0;
 // Maximum size of the chaining (collision resolution) array
 int max = 10;
 
 // Function Declarations
-node_t create_node(int key, int data);
 void init_hash_table();
 int hash_func(int key);
 void insert(int key, int data);
-void remove(int key);
-int find(int key, linked_list hash_table);
+void remove_node(int key);
+int find(int key, struct node *list);
 
 /**
  * Creates a node for insertion into the hash table
@@ -68,20 +67,20 @@ int find(int key, linked_list hash_table);
  * @param value: int
  * @return node_t
  */
-node_t create_node(int key, int data) {
-    node * temp = (struct node *) malloc(sizeOf(struct node));
-    temp->key = key;
-    temp->value = data;
-    temp->next = NULL;
-    return temp;
-}
+//struct node_t * create_node(int key, int data) {
+//    struct node_t *temp = malloc(sizeof(struct node_t));
+//    temp->key = key;
+//    temp->value = data;
+//    temp->next = NULL;
+//    return temp;
+//}
 
 /**
  * Initialize the hash table
  */
 void init_hash_table() {
     int k = 0;
-    for (k = 0; k < hash_table_size; k++) {
+    for (k = 0; k < HASH_TABLE_SIZE; k++) {
         hash_table[k].head = NULL;
         hash_table[k].tail = NULL;
     }
@@ -94,7 +93,17 @@ void init_hash_table() {
  * @return int
  */
 int hash_func(int key) {
-    return (int)((2971 * key + 101923) % 128189) % hash_table_size
+    return (int)((2971 * key + 101923) % 128189) % HASH_TABLE_SIZE;
+}
+
+struct node* get_element(struct node *list, int find_index) {
+    int i = 0;
+    struct node *temp = list;
+    while (i != find_index) {
+        temp = temp->next;
+        i++;
+    }
+    return temp;
 }
 
 /**
@@ -103,47 +112,50 @@ int hash_func(int key) {
  * @param key: int
  * @param data: struct node_t
  */
-void insert(int key, int data) {
+void insert(int key, int value) {
     // Keeps track of if you need to rehash
     float n = 0.0;
     // Find the hash index.
     int hash_index = hash_func(key);
     // Store the linked list from the hash table index
-    linked_list list = (struct node *) hash_table[hash_index].head;
+    struct node *list = (struct node*) hash_table[hash_index].head;
 
-    node * temp = create_node(key, data);
+    struct node *item = (struct node*) malloc(sizeof(struct node));
+    item->key = key;
+    item->value = value;
+    item->next = NULL;
 
     if (list == NULL) {
         // No linked list is present at the node.
-        hash_table[hash_index].head = temp;
-        hash_table[hash_index].tail = temp;
+        hash_table[hash_index].head = item;
+        hash_table[hash_index].tail = item;
         size++;
     }
     else {
         // There is a linked list present at the hash table node
-        int find_index = find(key,*list);
+        int find_index = find(key, list);
         if (find_index == -1) {
             // The key is not already present in the chaining linked list
-            hash_table[hash_index].tail->next = temp;
-            hash_table[hash_index].tail = temp;
-            size + ;
+            hash_table[hash_index].tail->next = item;
+            hash_table[hash_index].tail = item;
+            size ++;
         }
         else {
             // There is a linked list at the hashed node
             // Update the value of the existing key
             //TODO: consider ... is this the best thing to do here?
             //TODO: will this just overwrite the existing node?
-            node * pre_existing_node = find(find_index, list);
-            pre_existing_node->value = data;
+            struct node *element = get_element(list, find_index);
+            element->value = value;
         }
     }
     //TODO: refactor this
     //TODO: consider -- maybe we don't need this functionality
-    n = (1.0 * size) / max;
-    if (n >= 0.75) {
-        // rehashing
-        rehash();
-    }
+//    n = (1.0 * size) / max;
+//    if (n >= 0.75) {
+//        // rehashing
+//        rehash();
+//    }
 }
 
 /**
@@ -151,11 +163,11 @@ void insert(int key, int data) {
  * Removes an entry from the hash table.
  * @param value: struct node_t
  */
-void remove(int key) {
+void remove_node(int key) {
     // Hash the key
     int hash_index = hash_func(key);
     // Copy the linked list at the hash table index
-    linked_list list = (struct node*) hash_table[hash_index].head;
+    struct node *list = (struct node*) hash_table[hash_index].head;
 
     if (list == NULL) {
         // There is no linked list at the hash table index.
@@ -173,7 +185,7 @@ void remove(int key) {
         }
         else {
             // The key is present in the linked list.
-            node temp = list;
+            struct node *temp = list;
             if (temp->key == key) {
                 // Remove the node from the chaining list.
                 hash_table[hash_index].head = temp->next;
@@ -188,7 +200,7 @@ void remove(int key) {
                 temp = temp->next;
             }
 
-            if (hash_table[hash_index]).tail = temp->next) {
+            if (hash_table[hash_index].tail == temp->next) {
                 // Remove the node by setting temp->next to null
                 temp->next = NULL;
                 hash_table[hash_index].tail = temp;
@@ -205,22 +217,24 @@ void remove(int key) {
 }
 
 /**
- * Find the value according to key in the hash table.
+ * Find the node according to the key in the hash table.
  * @param key: int
  * @param hash_table: struct linked_list_t
  * Returns -1 if the key is not found, otherwise returns the value associated with the key.
  */
-int find(int key, linked_list hash_table) {
+int find(int key, struct node* list) {
         int hash_index = hash_func(key);
 
+        int retval = 0;
         //TODO: refactor the hash_table->entries
-        node current = hash_table->entries[hash_index];
+        struct node *temp = list;
 
-        while(current != NULL){
-            if (current->key == key){
-                return current->value;
+        while(temp != NULL){
+            if (temp->key == key){
+                return retval;
             }
-            current = current->next;
+            temp = temp->next;
+            retval++;
         }
         return -1; // Key not found//
 }
@@ -229,7 +243,9 @@ void display() {
 
 }
 
-int main() {
+void main() {
+    int key = 10;
+    int data = 500;
 
 }
 
